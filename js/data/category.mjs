5078d154e796548222cf51415b29b6107373bc0e
xenohim <xenohim@protonmail.com>
2022-03-27 16:07:29 +0200

    Balance.

--- js/data/category.mjs
+++ js/data/category.mjs
@@ -2,11 +2,12 @@ export default {
     "Townsfolk":              {color: "#55a630"},
     "Kingdom's Army":         {color: "#e63946", group: "Military"},
     "The Arcane Association": {color: "#C71585", group: "Arcane",
-                               requirement: [{task: "Concentration", requirement: 200}, {task: "Meditation", requirement: 200}], },
+                               requirement: [{skill: "Concentration", requirement: 200}, {skill: "Meditation", requirement: 200}], },
 
     "Fundamentals": {color: "#4a4e69"},
     "Combat":       {color: "#ff704d"},
-    "Magic":        {color: "#875F9A"},
+    "Magic":        {color: "#875F9A",
+                     requirement: [{skill: "Meditation", level: 1}], },
     "Dark magic":   {color: "#73000f",
                      requirement: [{rebirthEvil: 1}], },
 
